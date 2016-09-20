f0b28be74a5b4aa41347122f0ae4d8fb345f9a9c
James Sadler <freshtonic@gmail.com>
2016-09-20 16:57:06 +1000

    ColumnList renders but not editable yet.

--- src/components/ColumnList/ColumnList.js
+++ src/components/ColumnList/ColumnList.js
@@ -1,10 +1,30 @@
 import React, { Component, PropTypes } from 'react';
 
+const columnCountClasses = {
+  2: "list-vertical",
+  3: "list-vertical--thirds",
+  4: "list-vertical--fourths",
+};
+
 export default class ColumnList extends Component {
   render() {
+    let { block } = this.props;
     return (
       <div className="ColumnList">
-        Column list
+        <ul className={columnCountClasses[block.columns.length]}>
+          {
+            block.columns.map((column, index) => (
+              <li key={index}>
+                <article>
+                  <h3><a href='#'>{ column.heading }</a></h3>
+                  <p>
+                    { column.content }
+                  </p>
+                </article>
+              </li>
+            ))
+          }
+        </ul>
       </div>
     );
   }
--- src/containers/Editor/reducers/documentActions.js
+++ src/containers/Editor/reducers/documentActions.js
@@ -7,6 +7,10 @@ function makeBlock(kind, attrs) {
   return { id: uuid.v4(), kind, ...attrs };
 }
 
+function blurb() {
+  return "The Digital Transformation Office (DTO) was established as an executive agency in July 2015. Its mission is to lead the […]";
+}
+
 export const SIMPLE_BLOCKS = ["h1", "h2", "h3", "p", "blockquote"];
 
 export const appendBlock = (blockKind) => {
@@ -29,7 +33,11 @@ export const appendBlock = (blockKind) => {
       case "ColumnList":
         return {
           type: t.APPEND_BLOCK,
-          block: makeBlock("ColumnList", { columns: [{}, {}] })
+          block: makeBlock("ColumnList", { columns: [
+            {heading: "List item 1", content: blurb() },
+            {heading: "List item 2", content: blurb() },
+            {heading: "List item 3", content: blurb() },
+          ]})
         };
       default:
         console.warn(`Unknown block kind ${blockKind}`);
