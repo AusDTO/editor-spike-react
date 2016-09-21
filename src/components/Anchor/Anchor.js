55de1e4ecc4570ed570682b9539c678d45d75afa
Nathan Flew <nath@nathf.com>
2016-09-21 13:41:18 +1000

    Remove placeholder scenario from anchor

--- src/components/Anchor/Anchor.js
+++ src/components/Anchor/Anchor.js
@@ -3,10 +3,6 @@ import React from 'react'
 const Anchor = ({ block }) => {
   const { type, content, href = '#' } = block
   switch (type) {
-    case 'placeholder':
-      return (
-        <span className="placeholder-link">{content}</span>
-      )
     case 'seemore':
       return (
         <a className="see-more" href={href}>{content}</a>
